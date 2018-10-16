fd58487bcbb85d2147eab5040a6dd2fb54024b15
jegramin <jegroamin@gmail.com>
2018-10-16 23:57:48 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
+
+import styles from './styles.module.css'
 
 import Layout from '../components/layout'
 import Firebase from 'firebase';
-// import { stat } from 'fs';
+
 
 const FireApp = Firebase.initializeApp({
   apiKey: "AIzaSyD8ILoLnBCY8N-GrfXi4V7Br4b7UBT4spQ",
@@ -17,6 +18,61 @@ const FireApp = Firebase.initializeApp({
 
 const Firestore = FireApp.firestore();
 Firestore.settings({timestampsInSnapshots: true})
+
+
+class Header extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      signOut: ''
+    }
+  }
+  componentDidMount(){
+    Firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (!firebaseUser) {
+        this.setState({
+          signOut: 'none'
+        })
+      } else {
+        this.setState({
+          signOut: ''
+        })
+      } 
+    })
+  }
+  signOut = () => {
+    Firebase.auth().signOut()
+  }
+  render(){
+    return(
+      <div style={{
+        margin: '0 0 20px 0',
+        height: '90px',
+        backgroundColor: 'purple'
+      }}>
+      <button onClick={this.signOut} style={{
+          float: 'right',
+          margin: '15px 30px 0 0',
+          width: '110px',
+          backgroundColor: '#111e6c',
+          border: 'none',
+          color: '#ccc',
+          borderRadius: '5px',
+          display: this.state.signOut
+      }}>Sign Out</button>
+
+        <div style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '10px 0',
+          color: '#fff'
+        }}>
+          <h1>Blogging System</h1>
+        </div>
+      </div>
+    )
+  }
+}
 
 
 class SignUp extends React.Component{
@@ -49,31 +105,51 @@ class SignUp extends React.Component{
     Firebase.auth().onAuthStateChanged(firebaseUser => {
       if (firebaseUser) {
         this.setState({
-          blog: 'none'
+          blog: 'none',
+          component: ''
         })
       } else {
         this.setState({
-          component: 'none'
+          component: 'none',
+          blog: ''
         })
       }
     })
   }
   render(){
     return(
-      <div>
+      <div style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+      }}>
         <div style={{
           display: this.state.component
         }}>
           <Blogs />
         </div>
         <div style={{
-          display: this.state.blog
+          display: this.state.blog,
+          margin: '60px auto'
         }}>
-          <h1>{this.state.blog}</h1>
-          <input name={'email'} type={'email'} placeholder={'email'} onChange={this.emailChanged}></input>
-          <input name={'password'} type={'password'} placeholder={'password'} onChange={this.passwordChanged}></input>
-          <button name={'Sign In'} onClick={this.signIn}>Sign In</button>
-          <button name={'Sign Up'} onClick={this.CreateNewUser}>Sign Up</button>
+          <input name={'email'} type={'email'} placeholder={'email'} onChange={this.emailChanged} style={{
+            width: '55%',
+            margin: '5px auto',
+            border: '1px solid gray',
+            borderRadius: '4px',
+            padding: '5px',
+            backgroundColor: '#eee'
+          }}></input>
+          <input name={'password'} type={'password'} placeholder={'password'} onChange={this.passwordChanged} style={{
+            width: '55%',
+            margin: '5px auto',
+            border: '1px solid gray',
+            borderRadius: '4px',
+            padding: '5px',
+            backgroundColor: '#eee'
+          }}></input><br></br>
+          <button name={'Sign In'} onClick={this.signIn} style={{width: '95px',margin: '5px 0'}}>Sign In</button>
+          <button name={'Sign Up'} onClick={this.CreateNewUser} style={{width: '95px',margin: '5px 0'}}>Sign Up</button>
       </div>
       </div>
     )
@@ -134,7 +210,11 @@ class Blogs extends React.Component{
   }
   render(){
     return(
-      <div>
+      <div style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+      }}>
         <div style={{
           float: 'right',
           width: '30%'
@@ -161,10 +241,71 @@ class Blogs extends React.Component{
 }
 
 
+class Main extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      inputVal: '',
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    Firestore.collection('messages')
+      .onSnapshot(snap => {
+        this.setState({
+          list: snap.docs.map(doc => {
+            return {
+              ...doc.data()
+            }
+          })
+        })
+      })
+  }
+
+  onChange =(data)=>{
+    this.setState({
+      inputVal: data.target.value
+    })
+  }
+  getColumnsFromObject = obj => {
+    return Object.keys(obj).map(x => (
+      
+      <td>{obj[x]}</td> 
+      
+    ))
+  }
+  onEnter = event => {
+    if (event.keyCode === 13) {
+      if(this.state.inputVal){
+        Firestore.collection('messages')
+      .add({
+        message: this.state.inputVal
+      })
+      }
+    }
+  }
+  render(){
+    return(
+      <div className={styles.main}>
+      <table className={styles.messages}>
+          {
+             this.state.list.map(x => <tr>{this.getColumnsFromObject(x)}</tr>)
+          }
+        </table>
+        <input type='text' onChange={this.onChange} onKeyDown={this.onEnter} placeholder={'Group Chat'}></input>
+        
+      </div>
+    )
+  }
+}
+
 const IndexPage = () => (
   <Layout>
+    <Header />
     <SignUp />
-    <Link to="/page-2/">Go to page 2</Link>
+    <Main />
+    
   </Layout>
 )
 
